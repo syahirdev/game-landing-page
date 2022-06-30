4ee7b7f81a26e1971868cfcbabd1283a58035f60
syahirdev <888syahir@gmail.com>
2022-07-01 00:14:06 +0800

    import fonts

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900"
+            rel="stylesheet"/>
+          <link rel="icon" href="/favicon.svg"/>
+        </Head>
+        <body>
+        <Main/>
+        <NextScript/>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
