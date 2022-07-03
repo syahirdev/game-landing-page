d7ef8a4749309821fe9aaa3be4154360448d410c
syahirdev <888syahir@gmail.com>
2022-07-04 00:56:23 +0800

    seo & opengraph

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900"
             rel="stylesheet"/>
-          <link rel="icon" href="/favicon.svg"/>
+          <link rel="icon" href="/favicon.ico"/>
         </Head>
         <body>
         <Main/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,24 @@ import Faq from "../components/Faq";
 import Footer from "../components/Footer";
 import Cinematic from "../components/Cinematic";
 import Abstract from "../components/Abstract";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
+
+      {/*SEO & Opengraph*/}
+      <Head>
+        <title>Game Landing Page</title>
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        <meta name="description" content="An example game landing page"/>
+        <meta property="og:title" content="Game landing page"/>
+        <meta property="og:description" content="An example game landing page"/>
+        <meta property="og:url" content="https://www.game.syahir.dev/"/>
+        <meta property="og:image" content="https://game.syahir.dev/assets/images/hero.png"/>
+        <meta property="og:type" content="website"/>
+      </Head>
 
       {/*Header*/}
       <Header/>
